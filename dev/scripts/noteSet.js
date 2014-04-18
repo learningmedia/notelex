9d3cea3afa3c3e4bf2d4553581e2d43edf0b8f62
Andreas Helmberger <public@ahelmberger.de>
2014-04-18 23:44:34 +0200

    Remove Lodash and calculationHelper from noteSet

--- dev/scripts/noteSet.js
+++ dev/scripts/noteSet.js
@@ -1,30 +1,46 @@
-define(["lodash", "calculationHelper"], function(_, calculationHelper) {
+define([], function () {
+
+    function mod12(val) {
+        return ((val % 12) + 12) % 12;
+    }
+
+    function unique(arr) {
+        var hashes = Object.create(null);
+        return arr.reduce(function (uniqueValues, value) {
+            if (!hashes[value]) {
+                uniqueValues.push(value);
+                hashes[value] = true;
+            }
+            return uniqueValues;
+        }, []);
+    }
+
+    function sort(arr) {
+        var returnValue = arr.slice(0);
+        returnValue.sort(function (a, b) { return a - b });
+        return returnValue;
+    }
+
+    function shiftMod12(arr, offset) {
+        offset = offset || 0;
+        return arr.map(function (x) { return mod12(x + offset); });
+    }
 
     function NoteSet(values) {
-        var that = this;
-        if (!values) {
+        values = Array.prototype.slice.call(values || [], 0);
+
+        if (values.length === 0) {
             this.base = NaN;
             this.originalValues = [];
             this.intervals = [];
-        } else if (values.length === 0) {
-            this.base = NaN;
-            this.originalValues = [];
-            this.intervals = values;
         } else {
-            this.originalValues = Array.prototype.slice.call(values, 0);
-            values = _.sortBy(values, function(x) {
-                return x;
-            });
-            values = _.map(values, function(x) {
-                return calculationHelper.mod(x, 12);
-            });
-            values = _.unique(values);
+            this.originalValues = values.slice(0);
+            values = sort(values);
+            values = shiftMod12(values);
             this.base = values[0];
-            this.intervals = _.sortBy(_.map(values, function(x) {
-                return calculationHelper.mod(x - that.base, 12);
-            }), function(x) {
-                return x;
-            });
+            values = shiftMod12(values, -this.base);
+            values = unique(values);
+            this.intervals = sort(values)
         }
     }
 
