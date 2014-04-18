86cbd8e148e943784bda96bebaf5dd93d93b4a34
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2014-04-18 22:36:35 +0200

    Implement output for triads

--- dev/scripts/providers/functionProvider.js
+++ dev/scripts/providers/functionProvider.js
@@ -1,22 +1,15 @@
 define(["providers/providerHelper"], function (providerHelper) {
 
     return function(noteSet, language) {
-
-            var returnValue = "Grundton: ";
-            var base = providerHelper.getToneName(noteSet.base);
-            if (base) {
-                returnValue += base + "<br/>";
+    		console.log(noteSet.base);
+            var returnValue = "Name: ";
+            if (noteSet.base) {
+                if (noteSet.intervals.length === 3) {
+                returnValue += providerHelper.getTriadName(noteSet.base, noteSet.intervals).join("-");
+            	}
             }
             else{
-                returnValue += "unbestimmt<br/>";
-            }
-            if (noteSet.intervals.length === 3) {
-                console.log(noteSet.intervals);
-                var values = providerHelper.getGenusAndBehavior(noteSet.intervals);
-                returnValue += "<div>"+ 
-                "Tongeschlecht: " + values[0] + "<br/>" +
-                "Akkordgestalt: " + values[1] + "<br/>" +
-                "</div>";
+                returnValue += "unbestimmt";
             }
 
             return language === "de" ? returnValue : null;
--- dev/scripts/providers/providerHelper.js
+++ dev/scripts/providers/providerHelper.js
@@ -1,46 +1,45 @@
-define([], function () {
+define(["calculationHelper"], function (calculationHelper) {
 
-    function getGenusAndBehavior(intervals){
-        if (intervals[1] === 4 && intervals[2] === 7) {
-            return ["Dur", "Grundstellung"];
-        };
-        if (intervals[1] === 3 && intervals[2] === 8) {
-            return ["Dur", "Sextakkord"];
-        };
-        if (intervals[1] === 5 && intervals[2] === 9) {
-            return ["Dur", "Quartsextakkord"];
-        };
-        if (intervals[1] === 3 && intervals[2] === 7) {
-            return ["Moll", "Grundstellung"];
-        };
-        if (intervals[1] === 4 && intervals[2] === 9) {
-            return ["Moll", "Sextakkord"];
-        };
-        if (intervals[1] === 5 && intervals[2] === 8) {
-            return ["Moll", "Quartsextakkord"];
-        };   
+    function getTriadName(base, intervals){
+        var value = intervals.join("");
+        switch(value){
+            case "047": 
+                return [getToneName(base), "Dur", "Grundakkord"];
+            case "038":
+                return [getToneName(calculationHelper.mod(base + 8, 12)), "Dur", "Sextakkord"];
+            case "059":
+                return [getToneName(calculationHelper.mod(base + 5, 12)), "Dur", "Quartsextakkord"];
+            case "037": 
+                return [getToneName(base), "Moll", "Grundakkord"];
+            case "049":
+                return [getToneName(calculationHelper.mod(base + 9, 12)), "Moll", "Sextakkord"];
+            case "058":
+                return [getToneName(calculationHelper.mod(base + 5, 12)), "Moll", "Quartsextakkord"];
+            default:
+                return [];
+        }
     }
 
     function getToneName(midiValue){
         switch(midiValue){
             case 0: return "C";
-            case 1: return "Des (enhamonische Lesart notwendig)";
+            case 1: return "Des";
             case 2: return "D";
-            case 3: return "Es (enhamonische Lesart notwendig)";
+            case 3: return "Es";
             case 4: return "E";
             case 5: return "F";
-            case 6: return "Fis (enhamonische Lesart notwendig)";
+            case 6: return "Fis";
             case 7: return "G";
-            case 8: return "As (enhamonische Lesart notwendig)";
+            case 8: return "As";
             case 9: return "A";
             case 10: return "B";
-            case 11: return "Hs";
+            case 11: return "H";
         }
         return midiValue;
     }
 
     return {
-        getGenusAndBehavior: getGenusAndBehavior,
+        getTriadName: getTriadName,
         getToneName: getToneName
     };
     
