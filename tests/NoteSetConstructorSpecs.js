cbb7bb2226e9071dc14a00ada3b69f020afce4d5
Andreas Helmberger <public@ahelmberger.de>
2014-04-19 17:46:48 +0200

    Refactor NoteSet module to be a simple factory function

--- tests/NoteSetConstructorSpecs.js
+++ tests/NoteSetConstructorSpecs.js
@@ -1,53 +1,53 @@
-describe("NoteSet constructor function", function() {
+describe("noteSet function", function() {
 
-    var NoteSet;
+    var noteSet;
     var async = new AsyncSpec(this);
     
     async.beforeEach(function (done) {
-        require(["noteSet"], function(_NoteSet) {
-            NoteSet = _NoteSet;
+        require(["noteSet"], function(_noteSet) {
+            noteSet = _noteSet;
             done();
         });
     });
 
-    it("should return an object", function() {
-        var result = new NoteSet();
-        expect(result).toBeDefined();
+    it("should return a defined value.", function() {
+        var set = noteSet();
+        expect(set).toBeDefined();
     });
 
     it("should create an empty set if no argument is supplied.", function () {
-        var noteSet = new NoteSet();
-        expect(isNaN(noteSet.base)).toBe(true);
-        expect(noteSet.originalValues).toEqual([]);
-        expect(noteSet.intervals.length).toBe(0);
+        var set = noteSet();
+        expect(isNaN(set.base)).toBe(true);
+        expect(set.originalValues).toEqual([]);
+        expect(set.intervals.length).toBe(0);
     });
 
     it("should create an empty set if an empty array is supplied as argument.", function () {
-        var noteSet = new NoteSet([]);
-        expect(isNaN(noteSet.base)).toBe(true);
-        expect(noteSet.originalValues).toEqual([]);
-        expect(noteSet.intervals.length).toBe(0);
+        var set = noteSet([]);
+        expect(isNaN(set.base)).toBe(true);
+        expect(set.originalValues).toEqual([]);
+        expect(set.intervals.length).toBe(0);
     });
 
     it("should keep smallest value as base value.", function () {
-        var noteSet = new NoteSet([9, 5, 7, 17, 5]);
-        expect(noteSet.base).toBe(5);
+        var set = noteSet([9, 5, 7, 17, 5]);
+        expect(set.base).toBe(5);
     });
 
     it("should transpose base value to a value between 0 and 11 for values greater than 11.", function () {
-        var noteSet = new NoteSet([14, 16, 18]);
-        expect(noteSet.base).toBe(2);
+        var set = noteSet([14, 16, 18]);
+        expect(set.base).toBe(2);
     });
 
     it("should transpose base value to a value between 0 and 11 for values less than 0.", function () {
-        var noteSet = new NoteSet([-3, 0, 4]);
-        expect(noteSet.base).toBe(9);
+        var set = noteSet([-3, 0, 4]);
+        expect(set.base).toBe(9);
     });
 
     it("should normalize and order all values.", function () {
-        var noteSet = new NoteSet([9, 5, 7, 17, 5]);
-        expect(noteSet.originalValues).toEqual([9, 5, 7, 17, 5]);
-        expect(noteSet.intervals).toEqual([0, 2, 4]);
+        var set = noteSet([9, 5, 7, 17, 5]);
+        expect(set.originalValues).toEqual([9, 5, 7, 17, 5]);
+        expect(set.intervals).toEqual([0, 2, 4]);
     });
 
 });
--- tests/PcSetSpecs.js
+++ tests/PcSetSpecs.js
@@ -1,38 +1,38 @@
-describe("PcSetTable getPcSet function", function() {
+describe("pcSet function", function() {
 
-    var NoteSet;
+    var noteSet;
     var pcSet;
     var async = new AsyncSpec(this);
     
     async.beforeEach(function (done) {
-        require(["noteSet", "providers/pcSet"], function(_NoteSet, _pcSet) {
-            NoteSet = _NoteSet;
+        require(["noteSet", "providers/pcSet"], function (_noteSet, _pcSet) {
+            noteSet = _noteSet;
             pcSet = _pcSet;
             done();
         });
     });
 
     it("should calculate an empty PC set set for an empty NoteSet.", function () {
-        var noteSet = new NoteSet([]);
-        var set = pcSet(noteSet);
+        var notes = noteSet([]);
+        var set = pcSet(notes);
         expect(set.fortePrimeForm).toBe("");
     });
 
     it("should calculate 0,3,7 for a d major chord.", function () {
-        var noteSet = new NoteSet([2, 6, 9]);
-        var set = pcSet(noteSet);
+        var notes = noteSet([2, 6, 9]);
+        var set = pcSet(notes);
         expect(set.fortePrimeForm).toBe("037");
     });
 
     it("should calculate 0,2,6 for F,G,H.", function () {
-        var noteSet = new NoteSet([5, 7, 11]);
-        var set = pcSet(noteSet);
+        var notes = noteSet([5, 7, 11]);
+        var set = pcSet(notes);
         expect(set.fortePrimeForm).toBe("026");
     });
 
     it("should calculate 0,1,2,3,4,5,6,7,8,9,A for C-C#-D-Eb-E-F-F#-G-Ab-A-Bb.", function () {
-        var noteSet = new NoteSet([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-        var set = pcSet(noteSet);
+        var notes = noteSet([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        var set = pcSet(notes);
         expect(set.fortePrimeForm).toBe("0123456789A");
     });
 
