c3ae4761a081062d286e06795bc5c0b3db309291
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2014-04-19 16:47:35 +0200

    Implement first function output for C-Dur

--- dev/scripts/providers/functionProvider.js
+++ dev/scripts/providers/functionProvider.js
@@ -3,19 +3,68 @@ define(["providers/providerHelper"], function (providerHelper) {
     return function(noteSet, language) {
 
     		var message = " (enharmonische Lesart ist erforderlich)";
-            var returnValue = "Name: "; 
+            var returnValue, 
+            values,
+            valuesOutput,
+            enharmonicMessage,
+            name,
+            chord,
+            majorFunctions,
+            minorFunctions;
+
             if (!isNaN(noteSet.base)) {
 
-	                var values = providerHelper.getTriadName(noteSet.base, noteSet.intervals);
-	                var temp = providerHelper.enharmonic(values[0], values[1]) ? message : "";         
-	                returnValue += values.join("-") + temp;
-                    
+                if (noteSet.intervals.length === 3) {
+	                values = providerHelper.getTriadName(noteSet.base, noteSet.intervals);
+                    valuesOutput = values.join("-");
+                    enharmonicMessage = providerHelper.enharmonic(values[0], values[1]) ? message : "";
+                    name = valuesOutput + " " + enharmonicMessage;
+                    chord = values[0] + "-" + values[1];
+                    majorFunctions = getMajorFunctions(chord);
+                    minorFunctions = getMinorFunctions(chord);
+                }
+                        
+                returnValue = getNameObject(name);  
+                returnValue += getFunctionObject(chord, majorFunctions, minorFunctions); 
+                              
             }
             else{
                 returnValue += "unbestimmt";
             }
 
             return language === "de" ? returnValue : null;
+            
         };
 
+        function getMajorFunctions(chord){
+            switch(chord){
+                case "C-Dur":
+                    return "1. T = Tonika<br/>2. (D) S = Zwischendominante zur Subdominante"; 
+                default:
+                    return chord;
+            }
+        }
+
+        function getMinorFunctions(chord){
+            switch(chord){
+                case "C-Dur":
+                    return "1. T = Gleichnamige Durtonart</br>2. (D) s = Zwischendominante zur Subdominante"; 
+                default:
+                    return chord;
+            }
+        }
+
+        function getNameObject(name){
+            return "<div style='font-weight:bold;'>Name: " + name + "</div>";
+        }
+
+        function getFunctionObject(chord, majorFunctions, minorFunctions){
+            var value = "<hr/>";
+            value += "<div style='font-weight:bold;'>Mögliche Funktionen in " + chord + ":</div>";
+            value += "<div style='margin-left: 20px;'>" + majorFunctions + "</div>";
+            value += "<div style='font-weight:bold;'>Mögliche Funktionen in :</div>";
+            value += "<div style='margin-left: 20px;'>" + minorFunctions + "</div>";
+            return value;
+        }
+       
 });
