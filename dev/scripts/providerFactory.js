10be14e5e4d75b70d61951c57d180384075eb76d
Andreas Helmberger <public@ahelmberger.de>
2014-04-19 18:19:40 +0200

    Javascript language optimizations

--- dev/scripts/providerFactory.js
+++ dev/scripts/providerFactory.js
@@ -4,20 +4,20 @@ define(["providers/functionProvider"], function (functionProvider) {
         getProviders: function () {
             return [
                 {
-                    getName: function(language) { return "Allgemeine Musiklehre"; },
-                    getHeader: function(language) { return "AM"; },
+                    getName: function (language) { return "Allgemeine Musiklehre"; },
+                    getHeader: function (language) { return "AM"; },
                     getContent: function () { return null; }
                 }, {
-                    getName: function(language) { return "Funktionstheorie"; },
-                    getHeader: function(language) { return "FT"; },
+                    getName: function (language) { return "Funktionstheorie"; },
+                    getHeader: function (language) { return "FT"; },
                     getContent: functionProvider
                 }, {
-                    getName: function(language) { return "Pitch Class Set"; },
-                    getHeader: function(language) { return "PC"; },
+                    getName: function (language) { return "Pitch Class Set"; },
+                    getHeader: function (language) { return "PC"; },
                     getContent: function () { return null; }
                 }, {
-                    getName: function(language) { return "Tonfelder"; },
-                    getHeader: function(language) { return "TF"; },
+                    getName: function (language) { return "Tonfelder"; },
+                    getHeader: function (language) { return "TF"; },
                     getContent: function () { return null; }
                 }
             ];
